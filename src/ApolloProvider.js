fdf5a306fd030f67d363f506408af462bdf534fc
SpraylnlPray <danielwildegger@gmx.de>
2020-05-15 07:52:58 +0200

    Delete docker files, try CORS options

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -41,6 +41,8 @@ const cache = new InMemoryCache( {
 
 const client = new ApolloClient( {
 	link: new HttpLink( {
+		// uri: 'http://localhost:8080/graphql',
+		credentials: 'same-origin',
 		uri: 'http://ec2co-ecsel-1y0tuf5jfvvua-14597932.us-east-2.elb.amazonaws.com:8080/graphql',
 	} ),
 	cache,
