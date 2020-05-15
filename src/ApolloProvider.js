04208b782d2d0ab6af6b4a8cdd9ffc6f51c413ec
SpraylnlPray <danielwildegger@gmx.de>
2020-05-15 09:04:37 +0200

    CORS next

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -43,7 +43,6 @@ const client = new ApolloClient( {
 	link: new HttpLink( {
 		// uri: 'http://localhost:8080/graphql',
 		uri: 'http://ec2co-ecsel-1y0tuf5jfvvua-14597932.us-east-2.elb.amazonaws.com:8080/graphql',
-		credentials: 'same-origin',
 	} ),
 	cache,
 	resolvers: {
