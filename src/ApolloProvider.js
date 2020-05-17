8d4a2f852cb4634ed95cf6680b76d47620c7f2fd
SpraylnlPray <danielwildegger@gmx.de>
2020-05-17 23:29:57 +0200

    cors header

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -43,6 +43,9 @@ const client = new ApolloClient( {
 	link: new HttpLink( {
 		// uri: 'http://localhost:8080/graphql',
 		uri: 'https://server.methodical.software/graphql',
+		headers: {
+			"Access-Control-Allow-Origin": "*"
+		}
 	} ),
 	cache,
 	resolvers: {
