433e3a7c52bb55e0533e8ba8200a2e33bdf0050e
SpraylnlPray <danielwildegger@gmx.de>
2020-05-18 21:10:36 +0200

    remove uri logging

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -40,7 +40,7 @@ const cache = new InMemoryCache( {
 } );
 
 const uri = process.env.REACT_APP_ENV === 'prod' ? process.env.REACT_APP_PROD_HOST : process.env.REACT_APP_DEV_HOST;
-console.log('uri is ', uri);
+
 const client = new ApolloClient( {
 	link: new HttpLink( {
 		uri,
